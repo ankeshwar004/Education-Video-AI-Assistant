e53cc71677e617dee5edb4b0bf629db09a9dc95e
Ankeshwar <ankeshwar04@gmail.com>
2026-07-17 23:42:29 +0530

    fixig cv2 bug

--- src/ocr.py
+++ src/ocr.py
@@ -1,11 +1,10 @@
 import cv2
 import numpy as np
-import torch
 
 
 def ocr_frame(frame_pil, reader):
-    frame_rgb=cv2.cvtColor(np.array(frame_pil),cv2.COLOR_BGR2RGB)
-    result=reader.readtext(frame_rgb,detail=0)  # detail=0 returns only text
+    frame_bgr=cv2.cvtColor(np.array(frame_pil),cv2.COLOR_RGB2BGR)  
+    result=reader.readtext(frame_bgr,detail=0)
     if not result:
         return ""
     
