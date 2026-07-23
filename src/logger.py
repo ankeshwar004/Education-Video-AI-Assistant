323e626911a147f983b3478a861a9869b685a6b1
Ankeshwar <ankeshwar04@gmail.com>
2026-07-23 16:23:20 +0530

    "Minor Fix"

--- src/logger.py
+++ src/logger.py
@@ -5,7 +5,7 @@ from config import LOG_DIR
 def get_logger(name="VideoAssistant"):
     LOG_DIR.mkdir(parents=True,exist_ok=True)
     logging.basicConfig(
-        level=logging.INFO,
+        level=logging.WARNING,
         format="%(asctime)s %(levelname)s %(name)s %(message)s",
         handlers=[logging.FileHandler(LOG_DIR/"app.log"),
                   logging.StreamHandler()]
