7d871a53e8de3bb4e427411b16aab9405bfad8b3
Ankeshwar Raj <ankeshwar04@gmail.com>
2026-08-31 16:48:47 +0000

    "Add deduplication"

--- evaluation/reranker_eval.py
+++ evaluation/reranker_eval.py
@@ -2,7 +2,8 @@ from langsmith import traceable
 
 import config
 import os 
-from src.utils import save_json
+import numpy as np
+from src.utils import save_json, deduplication
 from evaluation.utils import compute_mrr, hit_rate_at_k, get_rank
 from src.retrieval import rerank
 from src.logger import get_logger
@@ -13,6 +14,7 @@ logger=get_logger(__name__)
 def reranker_result_summary(results):
     pre_ranks=[r["pre_rank"] for r in results]
     post_ranks=[r["post_rank"] for r in results]
+    dedup_ranks=[r["dedup_rank"] for r in results]
 
     valid_moves=[ r["movement"] for r in results if r["movement"] is not None]
 
@@ -24,23 +26,69 @@ def reranker_result_summary(results):
     if valid_moves:
         avg_move=sum(valid_moves) / len(valid_moves)
 
+
+    dedup_moves = [r["dedup_movement"]for r in results if r["dedup_movement"] is not None]
+
+    dedup_improved = sum(1 for m in dedup_moves if m > 0 )
+
+    dedup_worsened = sum(1 for m in dedup_moves if m < 0)
+
+    dedup_unchanged = sum(1 for m in dedup_moves if m == 0)
+
+    dedup_avg_move = None
+
+    if dedup_moves:
+        dedup_avg_move = sum(dedup_moves) / len(dedup_moves)
+    
+    total_retrieval_docs = sum(r["retrieval_docs"] for r in results)
+    total_dedup_docs = sum(r["dedup_docs"] for r in results)
+    total_removed_docs = sum(r["removed_docs"] for r in results)
+
+    removal_rate = (
+        total_removed_docs / total_retrieval_docs
+        if total_retrieval_docs
+        else 0
+    )
+
     summary = {
-        "mrr_before": compute_mrr(pre_ranks),
-        "mrr_after": compute_mrr(post_ranks),
-        "hit_rate_before": {
-            k: hit_rate_at_k(pre_ranks, k) for k in [1, 3, 5]
-        },
-        "hit_rate_after": {
-            k: hit_rate_at_k(post_ranks, k) for k in [1, 3, 5]
-        },
-        "rank_movement": {
+        "retrieval":{
+            "mrr": compute_mrr(pre_ranks),
+            "hit_rate":{
+                k: hit_rate_at_k(pre_ranks, k) for k in [1, 3, 5]
+            },
+        "reranker":{
+           "mrr": compute_mrr(post_ranks),
+            "hit_rate":{
+                k: hit_rate_at_k(post_ranks, k) for k in [1, 3, 5]
+            },  
+            "rank_movement": {
             "improved": improved,
             "worsened": worsened,
             "unchanged": unchanged,
             "average_movement": avg_move
-        },
-        "per_question": results
+                },
+            },
+        "reranker_with_dedup":{
+            "mrr": compute_mrr(dedup_ranks),
+            "hit_rate":{
+                k: hit_rate_at_k(dedup_ranks, k) for k in [1, 3, 5]
+            },
+            "rank_movement": {
+            "improved": dedup_improved,
+            "worsened": dedup_worsened,
+            "unchanged": dedup_unchanged,
+            "average_movement": dedup_avg_move
+                 },
+            },
+        "deduplication": {
+            "total_retrieval_docs": total_retrieval_docs,
+            "total_dedup_docs": total_dedup_docs,
+            "total_removed_docs": total_removed_docs,
+            "removal_rate": removal_rate
+            }    
+        }
     }
+
     
     
     return summary
@@ -50,6 +98,7 @@ def evaluate_reranker(qa_pairs,retrieval,video_id):
   results=[]
   retriever=retrieval['ensemble_retriever']
   reranker=retrieval['reranker']
+  embedder = retrieval['text_retriever'].vectorstore.embeddings
 
   for qa in qa_pairs:
 
@@ -58,24 +107,43 @@ def evaluate_reranker(qa_pairs,retrieval,video_id):
 
       # Before reranking
       pre_docs=retriever.invoke(query)
+
+      # similarities = get_pairwise_similarities(pre_docs, embedder)
+      # all_similarities.extend(similarities) 
+
       pre_rank=get_rank(target_start,pre_docs)
 
-      # After reranking
+      # Reranking without dedpulication 
       post_docs=rerank(query,pre_docs,reranker)
       post_rank=get_rank(target_start,post_docs)
+    
+      # Reranking with dedpulication 
+      dedup_docs=deduplication(pre_docs,embedder)
+      dedup_rerank_docs=rerank(query,dedup_docs,reranker)
+      dedup_rank=get_rank(target_start,dedup_rerank_docs)
+
 
       movement=None
 
       if pre_rank is not None and post_rank is not None:
           movement=pre_rank-post_rank
 
+      dedup_movement=None
+
+      if pre_rank is not None and dedup_rank is not None:
+        dedup_movement=pre_rank-dedup_rank 
+
       results.append({
           "question": query,
           "pre_rank": pre_rank,
           "post_rank": post_rank,
-          "movement": movement
+          "dedup_rank": dedup_rank,
+          "movement": movement,
+          "dedup_movement": dedup_movement,
+          "retrieval_docs": len(pre_docs),
+          "dedup_docs": len(dedup_docs),
+          "removed_docs": len(pre_docs)-len(dedup_docs)
       })
-  
   
   summary = reranker_result_summary(results)
   save_json(summary, os.path.join(config.RERANK_EVAL_RESULTS_DIR,f"{video_id}.json"))
--- src/utils.py
+++ src/utils.py
@@ -4,13 +4,27 @@ import json
 import os
 import tiktoken
 import config
-
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
 
 def format_time(seconds):
   hours=int(seconds//(60*60))
   minutes=int((seconds%(60*60))//60)
   seconds=int(seconds%60)
   return f"{hours:02d}:{minutes:02d}:{seconds:02d}" if hours>0 else f"{minutes:02d}:{seconds:02d}"
+
+
+def to_seconds(ts):
+    if isinstance(ts, (int, float)):
+        return float(ts)
+    parts = [float(p) for p in str(ts).split(':')]
+    seconds = 0.0
+    for p in parts:
+        seconds = seconds * 60 + p
+    return seconds
+  
+  
+  
 
 def is_similar(text,seen_texts,threshold=0.85):
   for seen_text in seen_texts:
@@ -43,3 +57,33 @@ def load_images(frames_metadata):
     with open(path,"rb") as f:
             images.append(base64.b64encode(f.read()).decode())
   return images
+
+
+
+def deduplication(docs, embedder, threshold=0.90):
+
+    unique_docs = []
+    unique_embeddings = []
+
+    embeddings = embedder.embed_documents(
+        [doc.page_content for doc in docs]
+    )
+
+    for doc, embedding in zip(docs, embeddings):
+
+        if not unique_embeddings:
+            unique_docs.append(doc)
+            unique_embeddings.append(embedding)
+            continue
+
+        similarities = cosine_similarity(
+            [embedding],
+            unique_embeddings
+        )[0]
+
+        if max(similarities) < threshold:
+            unique_docs.append(doc)
+            unique_embeddings.append(embedding)
+
+    return unique_docs
+
